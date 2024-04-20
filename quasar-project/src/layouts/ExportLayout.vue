64dd1b9ba8dabd242aa57339945ba3c433c173db
jacobpclouse <clouse.jp@gmail.com>
2024-04-20 12:33:10 -0400

    added logos to the me site and am pushing

--- quasar-project/src/layouts/ExportLayout.vue
+++ quasar-project/src/layouts/ExportLayout.vue
@@ -1,31 +1,35 @@
 <template>
-    <q-layout view="hHh lpR fFf">
-  
-      <q-header elevated class="bg-primary text-white">
-        <q-toolbar>
-          <q-toolbar-title>
-            <q-avatar>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <!-- <q-avatar>
               <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            Title
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-header>
-  
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-  
-      <q-footer elevated class="bg-grey-8 text-white">
-        <q-toolbar>
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            <div>Title</div>
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-footer>
-  
-    </q-layout>
-  </template>
+              <img src="~assets/logo2_black.svg">
+            </q-avatar> -->
+
+            <!-- limit height of the svg -->
+            <img src="~assets/logo2_black.svg" style="max-width: 7%; max-height: 7%;" alt="My Code Logo">
+
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
