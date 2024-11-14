98ebe85ada7cafcfef3ff5e56d7762dc3abad3c1
Jacob Clouse <clouse.jp@gmail.com>
2024-11-13 19:27:47 -0500

    kinda working

--- portfolio-proj/src/pages/IndexPage.vue
+++ portfolio-proj/src/pages/IndexPage.vue
@@ -12,24 +12,8 @@ import SkillsSection from "components/SkillsSection.vue";
 export default {
   name: "IndexPage",
   components: {
-    // HeroSection,
+    HeroSection,
     // SkillsSection,
   },
 };
 </script>
-
-<!-- <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
-</template>
-
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
-</script> -->
